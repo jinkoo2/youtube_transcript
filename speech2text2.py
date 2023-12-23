f18feaa73740a47a70d0e1161adb0e68817682ad
Jinkoo <jinkoo2@hotmail.com>
2023-12-22 19:11:05 -0500

    12/22/2023

--- speech2text2.py
+++ speech2text2.py
@@ -4,18 +4,18 @@ import wave
 import numpy as np
 
 # Load the pre-trained model
-model_file_path = './data/deepspeech-0.9.3-models.pbmm'  # Replace with the path to the model file
+model_file_path = './_data/deepspeech-0.9.3-models.pbmm'  # Replace with the path to the model file
 print('Loading model...'+model_file_path)
 model = deepspeech.Model(model_file_path)
 
-scorer_file_path = './data/deepspeech-0.9.3-models.scorer' 
+scorer_file_path = './_data/deepspeech-0.9.3-models.scorer' 
 if os.path.exists(scorer_file_path):
     print('Enabling score...'+scorer_file_path)
     model.enableExternalScorer(scorer_file_path)
 
 #audio_file = './data/audio/2830-3980-0043.wav'
 #audio_file = 'c:/Temp/streams/2/2.wav'
-audio_file = './data/converted.wav'
+audio_file = './_data/converted.wav'
 if not os.path.exists(audio_file):
     print('audio file not found...'+audio_file)
     exit(-1)
@@ -25,9 +25,15 @@ print('Loading audio file...'+audio_file)
 #    audio_data = f.read()
 
 with wave.open(audio_file, 'rb') as w:
+    rate = w.getframerate()
     frames = w.readframes(w.getnframes())
     audio_data = np.frombuffer(frames, dtype=np.int16)
 
+# Check if audio needs to be resampled
+print('rate=', rate)
+if rate != model.sampleRate():
+    raise ValueError(f"Invalid sample rate: {rate}. DeepSpeech model requires {model.sampleRate()}")
+    
 # Perform speech recognition
 print('Performing speech recognision...')
 text = model.stt(audio_data)
